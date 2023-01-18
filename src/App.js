fd51ef400a428ba3351bc0c9846dc8f5fce50ad7
Valentin <valentinarpin11@live.fr>
2023-01-18 14:33:28 +0100

    [Add] creation Routing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from "./nav/Navigation";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./vitrine/Home";
+import Valeurs from "./vitrine/Valeurs";
+import Contact from "./vitrine/Contact";
+import Blog from "./vitrine/Blog";
+import Footer from "./footer/Footer";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <BrowserRouter>
+            <Navigation/>
+            <Footer/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/valeurs" element={<Valeurs/>}/>
+                <Route path="/blog" element={<Blog/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+            </Routes>
+        </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/vitrine/Valeurs.jsx
@@ -0,0 +1,46 @@
+export default function Valeurs()
+{
+    return(
+        <div>
+            <div className="card">
+                <div className="card-header">
+                </div>
+                <div className="card-body">
+                    <blockquote className="blockquote mb-0">
+                        <p>LES VALEURS DE DYNASTY OF WO/MEN</p>
+                        <footer className="footer">is simply dummy text of the printing and typesetting
+                            industry. Lorem Ipsum has been the industry's standard dummy text ever since
+                            the 1500s, when an unknown printer took a galley of type and scrambled it to
+                            make a type specimen book. It has survived not only five centuries, but also
+                            the leap into electronic typesetting, remaining essentially unchanged. It was
+                            popularised in the 1960s with the release of Letraset sheets containing Lorem
+                            Ipsum passages, and more recently with desktop publishing software like Aldus
+                            PageMaker including versions of Lorem Ipsum.</footer>
+                    </blockquote>
+                </div>
+            </div>
+
+            <div className="card mb-3" style={{ maxWidth: 540 }}>
+                <div className="row no-gutters">
+                    <div className="col-md-4">
+                        <img src="" className="card-img" alt="" />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">
+                                This is a wider card with supporting text below as a natural lead-in
+                                to additional content. This content is a little bit longer.
+                            </p>
+                            <p className="card-text">
+                                <small className="text-muted">Last updated 3 mins ago</small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
